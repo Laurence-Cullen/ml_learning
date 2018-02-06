774185506d8b83350695256a1eafecf268c1e4e0
Laurence Cullen <laurence.cullen@uvue.xyz>
2018-02-06 20:58:32 +0000

    week 7 svm hw completed

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -97,17 +97,12 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
+    if ismember(str, vocabList)
+        index = strmatch(str, vocabList, 'exact');
+        word_indices = [word_indices ; index];
+    end
 
     % =============================================================
-
 
     % Print to screen, ensuring that the output lines are not too long
     if (l + length(str) + 1) > 78
